f17dad4aadd7f94af2894cc3fa67da1cb2c79c19
KonstantinDichev <kdichev.nl@gmail.com>
2024-01-26 12:54:47 +0100

    Add javadoc to the event service

--- src/main/java/com/javarun/web/services/EventServiceImpl.java
+++ src/main/java/com/javarun/web/services/EventServiceImpl.java
@@ -17,30 +17,54 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Implementation of the {@link IEventService} interface for managing events.
+ */
 @Service
 public class EventServiceImpl implements IEventService {
 
     private final EventRepository eventRepository;
     private final TeamRepository teamRepository;
 
+    /**
+     * Constructor to inject the EventRepository and TeamRepository dependencies.
+     *
+     * @param eventRepository The EventRepository instance to be injected.
+     * @param teamRepository  The TeamRepository instance to be injected.
+     */
     @Autowired
     public EventService(EventRepository eventRepository, TeamRepository teamRepository) {
         this.eventRepository = eventRepository;
         this.teamRepository = teamRepository;
     }
-
+    /**
+     * Get a list of all events.
+     *
+     * @return A list of EventDto representing all events.
+     */
     @Override
     public List<EventDto> getAllEvents() {
         List<Event> events = eventRepository.findAll();
         return events.stream().map(event -> mapToEventDto(event)).collect(Collectors.toList());
     }
+    /**
+     * Get details of a specific event by its ID.
+     *
+     * @param eventId The ID of the event to retrieve.
+     * @return The EventDto representing the requested event, or an empty Optional if not found.
+     */
 
     @Override
     public Optional<EventDto> getEventById(Long eventId) {
         Event event = eventRepository.findById(eventId).get();
         return mapToEventDto(event);
     }
-
+    /**
+     * Create a new event associated with a specific team.
+     *
+     * @param eventDto The EventDto representing the new event.
+     * @param teamId   The ID of the team associated with the event.
+     */
     @Override
     public void createEvent(EventDto eventDto, Long teamId ) {
         Team team = teamRepository.findById(teamId).get();
@@ -49,11 +73,21 @@ public class EventServiceImpl implements IEventService {
         eventRepository.save(event);
     }
 
+    /**
+     * Delete an event by its ID.
+     *
+     * @param eventId The ID of the event to delete.
+     */
     @Override
     public void deleteEvent(Long eventId) {
         eventRepository.deleteById(eventId);
     }
 
+    /**
+     * Update the details of an existing event.
+     *
+     * @param eventDto The EventDto representing the updated event details.
+     */
     @Override
     public void updateEvent(EventDto eventDto) {
         Event event = mapToEvent(eventDto);
