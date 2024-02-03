d207b68e7015e1f1906ae2844b88c19280547a95
KonstantinDichev <kdichev.nl@gmail.com>
2024-02-03 23:58:37 +0100

    enable async

--- src/main/java/com/javarun/web/WebApplication.java
+++ src/main/java/com/javarun/web/WebApplication.java
@@ -2,12 +2,12 @@ package com.javarun.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class WebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
-
 }
