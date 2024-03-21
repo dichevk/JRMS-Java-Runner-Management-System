ff8435208e0c0d9b9686a3c3654efffb4383eca1
KonstantinDichev <kdichev.nl@gmail.com>
2024-03-22 00:23:54 +0100

    Add viewer model

---
+++ src/main/java/com/javarun/web/models/Viewer.java
@@ -0,0 +1,27 @@
+package main.java.com.javarun.web.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import main.java.com.javarun.web.models.Event;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "viewers")
+public class Viewer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String passId; 
+    private String name;
+    private String country; 
+    private String iban; 
+    private Event event; 
+}
